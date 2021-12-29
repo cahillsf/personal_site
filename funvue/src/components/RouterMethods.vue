6f3046ecb9f030017f15d9b5b3eda5460f18152f
Steve Cahill <cahillsf9@gmail.com>
2021-12-29 17:42:34 -0500

    adding router object for global routing methods

---
+++ funvue/src/components/RouterMethods.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'RouterMethods',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    navigateToPage(route) {
+        this.$router.push({ path: route });
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
