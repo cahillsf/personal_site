e21823ba4e5a997edfb12c50dfdcb7daab481d75
cahillsf <stephen.cahill@datadoghq.com>
2022-02-21 21:46:32 -0500

    adding sh script for building/pushing imgs

---
+++ flask-server/docker_push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Hello world"
+
+while getopts r:t:i:p: flag
+do
+    case "${flag}" in
+        r) repo=${OPTARG};;
+        t) tag=${OPTARG};;
+        i) image_name=${OPTARG};;
+        p) path=${OPTARG};;
+    esac
+done
+echo "Repo: $repo";
+echo "Tag: $tag";
+echo "Image Name: $image_name";
+echo "Path: $path";
+
+docker build -t $image_name:$tag $path
+docker image tag $image_name:$tag $repo/$image_name:$tag
+docker image push $repo/$image_name:$tag
